9b818858315ccc01654b9ac9b21f54906d9f338e
Darryl Leung <darryl.leung@optimizely.com>
2024-06-06 16:22:51 +0200

    Tutorial checkpoint

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
-import { User } from "lucide-react";
-
 export default function Home() {
-    return <div>Dashboard Page</div>;
+
+    return (
+        <div>
+           Home Page
+        </div>
+    );
 }
